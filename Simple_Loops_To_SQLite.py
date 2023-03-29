e8080376785729b99ce313c6b03a5ce8f588cf53
blue-wild-angel <115838134+blue-wild-angel@users.noreply.github.com>
2023-03-29 11:14:23 -0400

    Update Simple_Loops_To_SQLite.py

--- Simple_Loops_To_SQLite.py
+++ Simple_Loops_To_SQLite.py
@@ -1,18 +1,21 @@
 # -*- coding: utf-8 -*-
 
 ############## Simple Script to Store a Folder of CSVs into a SQLite Database
+############## Using Pandas, glob & os
 
 import sqlite3
 import pandas as pd
 import glob
 import os
 
+# Specify your own folders
 db_save_to_folder = 'C:/MySaveFolder'
 Spreadsheets_Folder = 'C:/MySpreadsheetsFolder'
 
-#create database connection
+#create a SQLite database connection
 connection = sqlite3.connect('{}/BackUps.db'.format(db_save_to_folder))
 
+# This will loop through your folder full of CSVs
 for f in glob.glob("{}/*.csv".format(Spreadsheets_Folder)):
       base = os.path.basename(f)
       basename = os.path.splitext(base)[0]
