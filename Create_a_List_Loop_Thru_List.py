c640e6bef972e77f0d0626a6b2ef33e45887396d
blue-wild-angel <115838134+blue-wild-angel@users.noreply.github.com>
2023-04-04 10:47:39 -0400

    Add files via upload

---
+++ Create_a_List_Loop_Thru_List.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+#create a list of Names
+names = ['US Dollar','Euro','Australian Dollar','Japanese Yen']
+
+# use a for-loop to go through the list, print each name
+for name in names: 
+    print(name)
+
+print('List of Names printed')
